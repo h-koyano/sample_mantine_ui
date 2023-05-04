bdfdecab88109c6507243c05da022d74cdc8bd69
h-koyano <h.koyano@singular.co.jp>
2023-05-04 03:13:37 +0000

    hooks(use-Interval)

--- pages/hooks.tsx
+++ pages/hooks.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Layout } from '../components/Layout'
 import { Button, Center, Dialog, Group, Text, TextInput } from '@mantine/core'
-import { useDisclosure, useToggle } from '@mantine/hooks'
+import { useDisclosure, useInterval, useToggle } from '@mantine/hooks'
 import Link from 'next/link'
 import { ReplyIcon } from '@heroicons/react/solid'
+import { intervalToDuration } from 'date-fns'
 
 const MantineHooks = () => {
   const [opened, handlers] = useDisclosure(false, {
@@ -11,8 +12,28 @@ const MantineHooks = () => {
     onClose: () => console.log('Closed'),
   })
   const [btnColor, toggleBtnColor] = useToggle('yellow', ['yellow', 'violet'])
+  const [seconds, setSeconds] = useState(0)
+  const interval = useInterval(() => setSeconds((s) => s + 1), 1000)
+  useEffect(() => {
+    interval.start()
+    return interval.stop
+  }, [])
+
   return (
     <Layout title="Hooks">
+      <Group position="center" direction="column">
+        <Text>
+          This page is active <strong>{seconds}</strong> [s] after mount
+        </Text>
+        <Button
+          compact
+          onClick={interval.toggle}
+          color={interval.active ? 'red' : 'teal'}
+          variant="light"
+        >
+          {interval.active ? 'Suspend' : 'Activate'}
+        </Button>
+      </Group>
       <Dialog
         opened={opened}
         withCloseButton
